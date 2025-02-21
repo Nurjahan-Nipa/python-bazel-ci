ef64e8c7189a229bcb4524fd2c16d60abc38ca8d
Nurjahan Nipa <nurjahannipa@gmail.com>
2025-02-21 00:42:22 -0600

    clearing things

--- project-root/bazel-project-root/scripts/lint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-
-pip install pylint
-pylint src/*.py tests/*.py --fail-under=8.0
--- project-root/bazel-project-root/src/string_utils.py
+++
@@ -1,8 +0,0 @@
-def reverse_string(s):
-    return s[::-1]
-
-def is_palindrome(s):
-    return s 
-
-def count_vowels(s):
-    return sum(1 for char in s.lower())
