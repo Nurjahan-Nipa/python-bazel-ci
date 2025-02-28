c6003e66adf986c971df5e8891bf8e53f1d3335d
Nurjahan Nipa <nurjahannipa@gmail.com>
2025-02-28 14:19:33 -0600

    Fix executable permission for format.sh 

--- project-root/bazel-project-root/external/bazel_tools~cc_configure_extension~local_config_cc/get_env.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\VCVARSALL.BAT" amd64  -vcvars_ver=14.43.34808 > NUL 
-echo PATH=%PATH%,INCLUDE=%INCLUDE%,LIB=%LIB%,WINDOWSSDKDIR=%WINDOWSSDKDIR% 
